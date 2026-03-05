3fb26685489fd9bd8489f8b63c5e47bf4d8c7ab2
Bruno Chaves Keller <brunochaveskeller1102@gmail.com>
2026-03-05 15:02:30 +0000

    Add database seed for default user

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -5,4 +5,28 @@ class GoalsController < ApplicationController
     render json: goals
   end
 
+  def create
+    goal = Goal.new(goal_params)
+
+    if goal.save
+      render json: goal, status: :created
+    else
+      render json: goal.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def goal_params
+    params.require(:goal).permit(
+      :title,
+      :description,
+      :specific,
+      :measurable,
+      :relevant,
+      :timeframe,
+      :user_id
+    )
+  end
+
 end
