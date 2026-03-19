e7f123548e6c1ebcafe10b1cf1691145e54ee1c0
Bruno Chaves Keller <brunochaveskeller1102@gmail.com>
2026-03-19 16:23:32 +0000

    Add destroy action for goals

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -15,6 +15,24 @@ class GoalsController < ApplicationController
     end
   end
 
+  def update
+    goal = current_user.goals.find(params[:id])
+
+    if goal.update(goal_params)
+        render json: goal
+    else
+        render json: goal.errors, status: :unprocessable_entity
+  end
+end
+
+def destroy
+  goal = current_user.goals.find(params[:id])
+  goal.destroy
+
+  head :no_content
+end
+
+
   private
 
   def goal_params
